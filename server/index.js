42bfc35737c87eceaf34e5a6d222de07747df725
muklesh <muklesh942@gmail.com>
2024-09-24 20:26:56 +0530

    change url

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://flixxit-pi.vercel.app/", "http://localhost:3000"],
     credentials: true,
   })
 );
